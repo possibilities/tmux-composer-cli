ebe54d52ad8e210c42ceeb99fe9524d5681150fb
Mike Bannister <notimpossiblemike@gmail.com>
2025-07-02 11:53:33 -0400

    Remove unused capturePane and resizeWindow functions from tmux-utils

--- src/core/tmux-utils.ts
+++ src/core/tmux-utils.ts
@@ -2,7 +2,6 @@ import { execSync, exec } from 'child_process'
 import { promisify } from 'util'
 import { getTmuxSocketString } from './tmux-socket.js'
 import type { TmuxSocketOptions } from './tmux-socket.js'
-import { TERMINAL_SIZES } from './constants.js'
 
 const execAsync = promisify(exec)
 
@@ -20,36 +19,6 @@ export async function listWindows(
   } catch {
     return []
   }
-}
-
-export async function capturePane(
-  sessionName: string,
-  windowName: string,
-  socketOptions: TmuxSocketOptions = {},
-): Promise<string> {
-  const socketArgs = getTmuxSocketString(socketOptions)
-  const { stdout } = await execAsync(
-    `tmux ${socketArgs} capture-pane -p -t ${sessionName}:${windowName}`,
-    {
-      encoding: 'utf-8',
-      maxBuffer: 10 * 1024 * 1024,
-    },
-  )
-  return stdout
-}
-
-export async function resizeWindow(
-  sessionName: string,
-  windowName: string,
-  width: number = TERMINAL_SIZES.big.width,
-  height: number = TERMINAL_SIZES.big.height,
-  socketOptions: TmuxSocketOptions = {},
-) {
-  const socketArgs = getTmuxSocketString(socketOptions)
-  await execAsync(
-    `tmux ${socketArgs} resize-window -t ${sessionName}:${windowName} -x ${width} -y ${height}`,
-    { encoding: 'utf-8' },
-  )
 }
 
 export function socketExists(socketOptions: TmuxSocketOptions = {}): boolean {
